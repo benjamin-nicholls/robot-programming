2c7e231d52acf5bc7a83997567df56852962e9a4
Ben <benjaminpnicholls@gmail.com>
2023-10-11 11:05:31 +0000

    updates

--- laserscan.py
+++ laserscan.py
@@ -9,9 +9,46 @@ from sensor_msgs.msg import LaserScan
 #   display this pose in Rviz
 
 
+class ScanPublisher(Node):
+    def __init__(self):
+        #self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+        
+        pass
+
+
+
+class Scanner(Node):
+    
+    def __init__(self):
+        # Callback called any time a new laser scan becomes available.
+        self.subscriber = self.create_subscription(LaserScan, "/scan", self.laserscan_callback, 10)
+    
+
+    def laserscan_callback(self, data):
+        ''' Find the closest laser scan reading. Return position. '''
+        # http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/LaserScan.html
+        min_range = data.range_min
+        index = data.ranges.index(min_range)
+        angle = data.angle_min + data.angle_increment * index
+
+        '''
+        range_min
+        find range_min within ranges[]
+        save index
+        angle = angle_min + angle_increment * index
+        use angle of robot + angle --> angle of closest point
+        use this angle & range to calculate position (pose) of this reading
+        convert into geometry_msgs/PoseStamped
+        '''
+        # listen to base link for localisation (not accurate)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
+    # do something here
+
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
