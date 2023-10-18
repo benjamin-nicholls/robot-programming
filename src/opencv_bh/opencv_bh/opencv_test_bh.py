27843fbe07acdf3249d75c53d961ae8fa0019d90
Ben <benjaminpnicholls@gmail.com>
2023-10-18 10:57:39 +0000

    work on image callback (not working)

--- src/opencv_bh/opencv_bh/opencv_test_bh.py
+++ src/opencv_bh/opencv_bh/opencv_test_bh.py
@@ -1,11 +1,10 @@
 import rclpy
 from rclpy.node import Node
 
-from cv2 import cvtColor, imshow, COLOR_BGR2GRAY
-
+from cv2 import namedWindow, inRange, imshow, waitKey
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-
+import numpy
 
 class ImageConverter(Node):
 
@@ -17,8 +16,26 @@ class ImageConverter(Node):
                                                 self.image_callback,
                                                 10)
     
-def image_callback(self, data):
-    pass
+    def image_callback(self, data):
+        '''
+        masks out any specific color in the image
+        '''
+        cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        #print(cv_image)
+        namedWindow('image window', 1)  # 1 is a flag to set to automatically resize
+        imshow('image window', cv_image)
+        ## THIS DOESN'T WORK THE WAY I THINK IT DOES
+        masked_image = inRange(cv_image, (0,0,0), (160,160,160))
+        print(numpy.array(masked_image))
+        namedWindow('masked window', 1)
+        imshow('masked window', masked_image)
+        # WRONG
+        new_image = numpy.logical_and(cv_image, masked_image)
+        namedWindow('new window', 1)
+        imshow('new window', new_image)
+        
+        waitKey(1)
+        
 
 
 def main(args=None):
